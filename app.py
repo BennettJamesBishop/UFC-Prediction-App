0daab95f958196fcab68d4ac354c96bfefd00ca2
BennettBishop <Bennettjamesbishop@gmail.com>
2024-07-15 19:20:34 -0700

    Make wording better when showing result / Add logo

--- app.py
+++ app.py
@@ -1,14 +1,6 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
 from joblib import load
-from sklearn.model_selection import  StratifiedKFold, GridSearchCV
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.pipeline import Pipeline
-from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import StandardScaler, OneHotEncoder
-from sklearn.compose import ColumnTransformer
-from joblib import dump
 from joblib import load
 
 # Load the pre-trained model and data
@@ -41,8 +33,14 @@ def create_matchup_data(fighter1, fighter2):
     
     return matchup_data
 
+
+left_co, cent_co,last_co = st.columns(3)
+with cent_co:
+    st.image('logo.jpg', caption=None, width=200, use_column_width=None)
+
 # Title
-st.title('UFC Fight Predictor')
+with cent_co:
+    st.title('UFC Fight Predictor')
 
 # Fighter Selection in columns
 col1, col2 = st.columns(2)
@@ -56,24 +54,27 @@ if st.button('Predict Winner'):
     if fighter1 != fighter2:
         matchup_data_1 = create_matchup_data(fighter1, fighter2) # Fighter 1 is Red, Fighter 2 is Blue
         matchup_data_2 = create_matchup_data(fighter2, fighter1) # Fighter 1 is Blue, Fighter 2 is Red
-        
-        # Use the final model's pipeline to preprocess the data and predict
+            
+            # Use the final model's pipeline to preprocess the data and predict
         processed_data_1 = final_model.named_steps['preprocessor'].transform(matchup_data_1)
         processed_data_2 = final_model.named_steps['preprocessor'].transform(matchup_data_2)
-        
-        # Predict the winner
+                
+                # Predict the winner
         prediction_proba_1 = final_model.named_steps['classifier'].predict_proba(processed_data_1)[0]
         prediction_proba_2 = final_model.named_steps['classifier'].predict_proba(processed_data_2)[0]
-       
-        #Average out two results (Eliminates bias towards blue fighters (data has 66/33 split of blue fighter winning,
-        #instead of retraining model this is my work around lol))
-        red_prediction_proba = (prediction_proba_1[0] + prediction_proba_2[1]) / 2
-        blue_prediction_proba = (prediction_proba_1[1] + prediction_proba_2[0]) / 2
-        winner = 'Red' if red_prediction_proba > blue_prediction_proba else 'Blue'
-        # Change Red and Blue to fighter1 and fighter2 above for better UI
+            
+                #Average out two results (Eliminates bias towards blue fighters (data has 66/33 split of blue fighter winning,
+                #instead of retraining model this is my work around lol))
+        fighter1_prediction_proba = (prediction_proba_1[0] + prediction_proba_2[1]) / 2
+        fighter2_prediction_proba = (prediction_proba_1[1] + prediction_proba_2[0]) / 2
+        winner = fighter1 if fighter1_prediction_proba > fighter2_prediction_proba else fighter2
+                # Change Red and Blue to fighter1 and fighter2 above for better UI
         st.subheader(f'The predicted winner is: {winner}')
-        st.write(f'Prediction probabilities - Red: {red_prediction_proba:.2f}, Blue: {blue_prediction_proba:.2f}')
-    else:
-        st.write('Please select two different fighters.')
+        if winner == fighter1:
+            st.write(f'{fighter1} has a {100 * fighter1_prediction_proba:.2f}% chance of winning')
+        else:
+            st.write(f'{fighter2} has a {100 * fighter2_prediction_proba:.2f}% chance of winning')
+else:
+    st.write('Please select two different fighters.')
 
 # To run this Streamlit app, save it in a file (e.g., app.py) and run `streamlit run app.py` in your terminal.
